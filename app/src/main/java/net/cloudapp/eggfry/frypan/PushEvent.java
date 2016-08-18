7a9ae631893a6f59dd3f9842e428ef540acde426
Lee <ljiho1998@naver.com>
2016-08-18 16:14:28 +0900

    commit

---
+++ app/src/main/java/net/cloudapp/eggfry/frypan/PushEvent.java
@@ -0,0 +1,16 @@
+package net.cloudapp.eggfry.frypan;
+
+/**
+ * Created by user on 2016-08-18.
+ */
+public class PushEvent {
+    private String string;
+
+    public PushEvent(String str) {
+       this.string = str;
+    }
+
+    public String getString() {
+        return string;
+    }
+}
--- app/src/main/java/net/cloudapp/eggfry/frypan/WaitingRoomActivity.java
+++ app/src/main/java/net/cloudapp/eggfry/frypan/WaitingRoomActivity.java
@@ -1,33 +1,37 @@
 package net.cloudapp.eggfry.frypan;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 public class WaitingRoomActivity extends AppCompatActivity {
 
-    public static boolean isActive = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_waiting_room);
 
-        isActive = true;
-
+        Intent recvIntent = getIntent();
         Toolbar toolbar = (Toolbar)findViewById(R.id.toolBar);
         setSupportActionBar(toolbar);
+        toolbar.setTitle(recvIntent.getStringExtra("channel") + "번 채널");
 
         ActionBar actionBar = getSupportActionBar();
         if (actionBar != null)
             actionBar.setDisplayHomeAsUpEnabled(true);
-
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
         BusProvider.getInstance().post(new PushEvent("Destroy"));
-        isActive = false;
+    }
+
+    // 준비 버튼 처리
+    public void onReadyBtnClicked(View v) {
+        BusProvider.getInstance().post(new PushEvent("Ready"));
     }
 }
