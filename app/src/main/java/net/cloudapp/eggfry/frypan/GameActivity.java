b8c8d80800d11a0248f6c76a65889ff36cc47457
identity16 <swj@wonjun-ui-MacBook-Air.local>
2016-08-27 10:38:54 +0900

    GameActivity

--- app/src/main/java/net/cloudapp/eggfry/frypan/GameActivity.java
+++ app/src/main/java/net/cloudapp/eggfry/frypan/GameActivity.java
@@ -1,23 +1,41 @@
 package net.cloudapp.eggfry.frypan;
 
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
 import com.bumptech.glide.Glide;
-import android.widget.ImageView;
+import com.bumptech.glide.request.animation.GlideAnimation;
+import com.bumptech.glide.request.target.ViewTarget;
+import com.squareup.otto.Subscribe;
 
-import com.bumptech.glide.Glide;
+import java.util.HashMap;
+
+
 
 public class GameActivity extends AppCompatActivity {
     private SoundManager soundManager;
     private static final int INITIALTIME = 3000;
     private static final int DURATION = 1336;
     private static final int DURATION_SMALL = 334;
+
+    ImageView[] def_beats;                  // 방어 시 누를 버튼
+    RelativeLayout defendContainer;
+    HashMap<String, Animation> animations = new HashMap<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,20 +46,51 @@ public class GameActivity extends AppCompatActivity {
 
         BusProvider.getInstance().register(this);
 
-        ImageView bg = (ImageView)findViewById(R.id.game_bg);
-        ImageView count1 = (ImageView)findViewById(R.id.count1);
-        ImageView count2 = (ImageView)findViewById(R.id.count2);
-        ImageView count3 = (ImageView)findViewById(R.id.count3);
-        ImageView count4 = (ImageView)findViewById(R.id.count4);
+        animations.put("in", AnimationUtils.loadAnimation(this, R.anim.in));
 
+        defendContainer = (RelativeLayout)findViewById(R.id.defend_container);
+
+        def_beats = new ImageView[]{
+                new ImageView(this),
+                new ImageView(this),
+                new ImageView(this),
+                new ImageView(this)
+        };
+
+        ImageView bg = (ImageView)findViewById(R.id.game_bg);
+
+        SquareLayout count1 = (SquareLayout) findViewById(R.id.count1);
+        SquareLayout count2 = (SquareLayout) findViewById(R.id.count2);
+        SquareLayout count3 = (SquareLayout) findViewById(R.id.count3);
+        SquareLayout count4 = (SquareLayout) findViewById(R.id.count4);
+
+        for (ImageView def_beat : def_beats) {
+            def_beat.setMaxWidth(20);
+            def_beat.setMaxHeight(20);
+
+            def_beat.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    // true, false
+                }
+            });
+
+            def_beat.setBackgroundResource(R.drawable.circle);      // 이미지 대신 임시로
+            // 이미지 있으면
+            Glide.with(this).load(R.drawable.beat1).asBitmap().into(def_beat);
+        }
 
         // Image Initialization
-        Glide.with(this).load(R.drawable.in_game_bg).into(bg);
-        Glide.with(this).load(R.drawable.ting).into(count1);
-        Glide.with(this).load(R.drawable.ting).into(count2);
-        Glide.with(this).load(R.drawable.tang).into(count3);
-        Glide.with(this).load(R.drawable.tang).into(count4);
+        Glide.with(this).load(R.drawable.in_game_bg).asBitmap().into(bg);
+        Glide.with(this).load(R.drawable.ting).asBitmap().into(new SquareLayoutTarget(this, count1));
+        Glide.with(this).load(R.drawable.ting).asBitmap().into(new SquareLayoutTarget(this, count2));
+        Glide.with(this).load(R.drawable.tang).asBitmap().into(new SquareLayoutTarget(this, count3));
+        Glide.with(this).load(R.drawable.tang).asBitmap().into(new SquareLayoutTarget(this, count4));
 
+        Glide.with(this).load(R.drawable.beat1).asBitmap().into(def_beats[0]);
+        Glide.with(this).load(R.drawable.beat2).asBitmap().into(def_beats[1]);
+        Glide.with(this).load(R.drawable.beat3).asBitmap().into(def_beats[2]);
+        Glide.with(this).load(R.drawable.beat4).asBitmap().into(def_beats[3]);
 
     }
 
@@ -53,37 +102,66 @@ public class GameActivity extends AppCompatActivity {
     }
 
 
+    /**
+     * 공격
+     */
+    public void attack() {
+        // 서버에 공격 정보 전송
 
+    }
+
+    /**
+     * 공격 받을 시 방어
+     * (서버에서 공격 신호를 받았을 때 실행)
+     * @param num 방어 횟수
+     */
+    public void defend(int num) {
+
+    }
+
+    /**
+     *  방어 시 배경 눌렸을 때(실패 처리)
+     * @param v defendContainer
+     */
+    public void onDefendLayoutClicked(View v) {
+        if(defendContainer.getChildCount() > 0) {
+            // 실패 처리
+            defendFailed();
+        }
+    }
+
+    /**
+     * 방어 실패
+     */
+    public void defendFailed() {
+
+    }
+
+
+    public void setCountView(int[] imageIds) {
+
+    }
+
+
+    // 애니메이션 테스트
     public void onUpImgClicked(View v) {
-        // 애니메이션 테스트(지워도 상관 X)
 
-        ImageView count1 = (ImageView)findViewById(R.id.count1);
-        ImageView count2 = (ImageView)findViewById(R.id.count2);
-        ImageView count3 = (ImageView)findViewById(R.id.count3);
-        ImageView count4 = (ImageView)findViewById(R.id.count4);
+        int interval = 100;
+        int[] squareLayoutIds = {R.id.count1, R.id.count2, R.id.count3,R.id.count4};
 
-        Animation animation = AnimationUtils.loadAnimation(this, R.anim.in);
+        int i = 1;
 
-        count1.startAnimation(animation);
-        count2.startAnimation(animation);
-        count3.startAnimation(animation);
-        count4.startAnimation(animation);
-    }
+        for (int viewId : squareLayoutIds) {
+            Animation animation = animations.get("in");
+            animation.setStartOffset(i * interval);
 
-    public void onLeftImgClicked(View v) {
-
-    }
-
-    public void onRightImgClicked(View v) {
-
-    }
-
-
-
-
-
-    public float speedCalculator(int speed) {
-        return (float)((100-speed)/100);
+            SquareLayout a = (SquareLayout)findViewById(squareLayoutIds[i-1]);
+            a.setBackgroundResource(R.drawable.circle);
+            Glide.with(this).load(R.drawable.lemon).asBitmap()
+                    .into(new SquareLayoutTarget(this, (SquareLayout)findViewById(squareLayoutIds[i-1])));
+            a.startAnimation(animation);
+            i++;
+        }
     }
 
     public void setSound() {
@@ -105,6 +183,8 @@ public class GameActivity extends AppCompatActivity {
         soundManager.loadSound("start4", R.raw.start_4);
         soundManager.loadSound("drum", R.raw.drumbit);
     }
+
+
     public void initialPlaySound() {
         Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
@@ -142,6 +222,8 @@ public class GameActivity extends AppCompatActivity {
             }
         }, INITIALTIME + DURATION * 4);
     }
+
+    // 숫자 -> 문자
     public String numberToString(int number) {
         String number_string = "";
         switch(number) {
@@ -209,4 +291,94 @@ public class GameActivity extends AppCompatActivity {
             }, initialTime +  (int)(DURATION * i * speed));
         }
     }
+
+    // 서버에서 메세지를 받았을 때
+    @Subscribe
+    public void FinishLoad(PushEvent mPushEvent) {
+        String[] message = mPushEvent.getString().split(" ");
+        switch (message[0]) {
+            case "Send":
+                Log.d("GameActivity", String.valueOf(SocketService.gameManager.getAttackCount()));
+                defend(SocketService.gameManager.getAttackCount());
+                break;
+            case "Time":
+                break;
+        }
+        if(message[0].equals("Send")) {
+
+        } else if(message[0].equals("Time")) {
+
+        }
+    }
+}
+
+
+
+
+// Glide를 SquareLayout에 적용시키기 위한 클래스
+class SquareLayoutTarget extends ViewGroupTarget<Bitmap> {
+    private Context context;
+
+    public SquareLayoutTarget(Context context, FrameLayout frameLayout) {
+
+        super(frameLayout);
+
+        this.context = context;
+    }
+
+    /**
+     * Sets the {@link Bitmap} on the view using
+     * {@link ImageView#setImageBitmap(Bitmap)}.
+     *
+     * @param resource The bitmap to display.
+     */
+
+    @Override
+    protected void setResource(Bitmap resource) {
+        ImageView imageView = (ImageView) ((ViewGroup) view.getChildAt(0)).getChildAt(0);
+        Drawable drawable = new BitmapDrawable(resource);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+            view.setBackground(drawable);
+        }
+    }
+
+}
+
+abstract class ViewGroupTarget<T> extends ViewTarget<ViewGroup, T> implements GlideAnimation.ViewAdapter {
+
+    public ViewGroupTarget(ViewGroup view) {
+        super(view);
+    }
+
+    @Override
+    public Drawable getCurrentDrawable() {
+        return view.getBackground();
+    }
+
+    @Override
+    public void setDrawable(Drawable drawable) {
+        view.setBackgroundDrawable(drawable);
+    }
+
+    @Override
+    public void onLoadStarted(Drawable placeholder) {
+        view.setBackgroundDrawable(placeholder);
+    }
+
+    @Override
+    public void onLoadFailed(Exception e, Drawable errorDrawable) {
+        view.setBackgroundDrawable(errorDrawable);
+    }
+
+    @Override
+    public void onLoadCleared(Drawable placeholder) {
+        view.setBackgroundDrawable(placeholder);
+    }
+
+    @Override
+    public void onResourceReady(T resource, GlideAnimation<? super T> glideAnimation) {
+        this.setResource(resource);
+    }
+
+    protected abstract void setResource(T resource);
 }
